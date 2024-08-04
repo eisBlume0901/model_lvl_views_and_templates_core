423548467389c1c313a80afe3ffae20149104e5e
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-08-04 20:20:40 +0800

    Created project model

--- proj_mgm/models.py
+++ proj_mgm/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import *
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT) # PROTECT means that if a user is deleted, the project will not be deleted
+    def __str__(self):
+        return self.name
